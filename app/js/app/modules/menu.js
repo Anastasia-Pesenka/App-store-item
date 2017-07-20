3d6d494f7bd5c2273e895bbb134629c792a4faa8
Anastasia-Pesenka <nastik1239999@gmail.com>
2017-07-20 10:27:21 +0300

    com6

--- app/js/app/modules/menu.js
+++ app/js/app/modules/menu.js
@@ -1,19 +1,36 @@
-define(['underscore', 'text!templates/menu.html', 'jquery', 'jqueryui'],
-    function (_,  menuTpl, $) {
+define(['fb', 'radio', 'underscore', 'text!templates/menu.html', 'jquery', 'jqueryui'],
+    function (fb, radio, _,  menuTpl, $) {
         return {
             init: function () {
                 this.template = _.template(menuTpl);
                 this.$el = $(".menu");
-
                 this.render();
                 this.renderTabs();
-            },
-            render: function () {
-                this.$el.html(this.template) ;
+                // this.setupEvents();
 
             },
-            renderTabs: function () {
+            render: function () {
+                this.$el.html(this.template()) ;
+            },
+            renderTabs : function () {
                 $( "#tabs" ).tabs();
+            },
+            setupEvents : function () {
+                radio.on('userSign', function (user) {
+                    console.log(user);
+                    // this.render(user);
+                }.bind(this));
+                this.$el.on('click', this.clickHandler.bind(this));
+            },
+            clickHandler : function (e) {
+                if($(e.target).is('.log-in')){
+                    fb.signIn();
+                }
+                if($(e.target).is('.log-out')){
+                    fb.signOut();
+                }
+
             }
-        };
+
+        }
     });
